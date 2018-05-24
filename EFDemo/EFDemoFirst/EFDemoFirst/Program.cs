7036a281eb3ab762c250ecd0079e65efca35f978
Goldsaints <710845067@qq.com>
2018-05-24 21:41:29 +0800

    add first

---
+++ EFDemo/EFDemoFirst/EFDemoFirst/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDemoFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //做一个添加操作
+            //1、声明 一个EF的上下文
+
+
+        }
+    }
+}
