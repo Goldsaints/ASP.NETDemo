f634220e7333c6c34736220f79f5ec33826bed8f
Goldsaints <710845067@qq.com>
2018-05-26 22:51:53 +0800

    update

--- EFDemo/EFDemoFirst/EFDemoFirst/Program.cs
+++ EFDemo/EFDemoFirst/EFDemoFirst/Program.cs
@@ -11,9 +11,63 @@ namespace EFDemoFirst
         static void Main(string[] args)
         {
 
-            //做一个添加操作
-            //1、声明 一个EF的上下文
+            //获取sql连接字符
+            string str = System.Configuration.ConfigurationManager.ConnectionStrings["MyTestEntities"].ConnectionString;
+            //把str 解密 
 
+            MyTestEntities dbContext = new MyTestEntities();
+
+            #region add
+            //News news = new News();
+            //news.Content = "content";
+            //news.TitleName = "TitleNmae";
+            //dbContext.News.Add(news);
+            //dbContext.SaveChanges();
+            #endregion
+
+            #region Update
+            //News news = new News();
+            //news.Content = "content";
+            //news.TitleName = "a22a";
+            //news.ID = 1;
+            //修改一行数据
+            //dbContext.Entry<News>(news).State = System.Data.EntityState.Modified;
+            // System.Data.EntityState.Detached 不跟踪实体，dbContext可以继续用，可以被回收
+
+            //只修改某一列
+            //dbContext.News.Attach(news);//附加到上下文操作来管理
+            //dbContext.Entry<News>(news).Property("TitleName").IsModified = true;
+            // 或者 dbContext.Entry<News>(news).Property<string>(u => u.TitleName).IsModified = true; 
+
+            #endregion
+
+            #region Delect
+            //News news = new News();
+            //news.ID = 3; 或者
+            //News news = new News() { ID = 3 };
+            //dbContext.Entry<News>(news).State = System.Data.EntityState.Deleted;
+            #endregion
+
+            #region SelectAll
+            //foreach (var news in dbContext.News) {
+            //    Console.WriteLine(news.ID + "" + news.Content);
+            //}
+            #endregion
+
+            #region Linq 查询
+            // u 遍历  dbContext.News 高效的sql脚本，NetFramework3.0后。 返回的类型为Iqueryable<News>
+            var temp = from n in dbContext.News
+                       where n.ID == 4
+                       select n;
+
+            foreach (var n in dbContext.News)
+            {
+                Console.WriteLine(n.ID + "" + n.Content);
+            }
+            #endregion
+
+            dbContext.SaveChanges();
+            Console.ReadKey();
 
         }
     }
